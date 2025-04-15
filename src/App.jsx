308fd0b3b6beec7c130061f0c981f56d61a2c112
Kemas Khairunsyah <khairunsyah2714@gmail.com>
2025-04-15 17:08:28 +0700

    SignUp, routing, validation input

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './Login'
+import SignUp from './SignUP'
 import './App.css'
 
 function App() {
 
   return (
-    <div>
-      <Login/>
-    </div>
+    // <div>
+    //   <Login/>
+    // </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} /> // buat route awal-awal, pertama kali buka web
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
   )
 }
 
