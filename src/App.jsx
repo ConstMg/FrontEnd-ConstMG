134828e27812b40e6429555b33f1a3b33d7209ea
Moch Rizqi <rizqia417@gmail.com>
2025-04-22 03:50:04 +0700

    adding project section

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './pages/Login'
-import Home from './pages/Home'
+import MainPage from './pages/MainPage'
 import './App.css'
 
 function App() {
 
   return (
-    // <div>
-    //   <Login/>
-    // </div>
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} /> // buat route awal-awal, pertama kali buka web
+        <Route path="/" element={<MainPage />} /> // buat route awal-awal, pertama kali buka web
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
