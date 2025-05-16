1dd3d1305aa9c66a380129cbb3230978a2174474
Kemas Khairunsyah <khairunsyah2714@gmail.com>
2025-05-17 03:49:27 +0700

    update endpoint

--- src/services/KaryawanServices.js
+++ src/services/KaryawanServices.js
@@ -1,79 +1,79 @@
 import { apiClient } from "./ApiClient";
 
 export const presensi = async (
+  nama,
+  status_presensi,
+  latitude,
+  longitude,
+  deskripsi
+) => {
+  return apiClient("karyawan/presensi", "POST", {
     nama,
     status_presensi,
     latitude,
     longitude,
-    deskripsi
-) => {
-    return apiClient("karyawan/presensi", "POST", {
-        nama,
-        status_presensi,
-        latitude,
-        longitude,
-        deskripsi,
-    });
+    deskripsi,
+  });
 };
 
 export const addKaryawan = async (
+  nama,
+  nik,
+  jk,
+  divisi,
+  alamat,
+  penempatan,
+  email,
+  password,
+  userRole
+) => {
+  return await apiClient(`admin/tambah/karyawan?akses=${userRole}`, "POST", {
     nama,
     nik,
     jk,
-    divisi,
     alamat,
+    divisi,
     penempatan,
     email,
     password,
-    userRole
-) => {
-    return await apiClient(`admin/tambah/karyawan?akses=${userRole}`, "POST", {
-        nama,
-        nik,
-        jk,
-        alamat,
-        divisi,
-        penempatan,
-        email,
-        password,
-    });
+  });
 };
 
 export const updateKaryawan = async (
-    id,
+  id,
+  nama,
+  nik,
+  jk,
+  divisi,
+  alamat,
+  penempatan,
+  email,
+  password,
+  userRole
+) => {
+  return await apiClient(`admin/karyawan/${id}?akses=${userRole}`, "PUT", {
     nama,
     nik,
     jk,
-    divisi,
     alamat,
+    divisi,
     penempatan,
     email,
     password,
-    userRole
-) => {
-    return await apiClient(`admin/karyawan/${id}?akses=${userRole}`, "PUT", {
-        nama,
-        nik,
-        jk,
-        alamat,
-        divisi,
-        penempatan,
-        email,
-        password
-    });
+  });
 };
 
 export const setRoleKaryawan = async (nama, role) => {
-    return await apiClient(`karyawan/update-role`, "PATCH", {
-        nama,
-        role,
-    });
+  return await apiClient(`karyawan/update-role`, "PATCH", {
+    nama,
+    role,
+  });
 };
 
 export const getKaryawan = async (role) => {
-    return await apiClient(`karyawan?role=${role}`, "GET");
+  return await apiClient(`admin/karyawan?akses=${role}`, "GET");
 };
 
 export const deleteKaryawan = async (id, role) => {
-    return await apiClient(`admin/karyawan/${id}?akses=${role}`, "DELETE");
+  return await apiClient(`admin/karyawan/${id}?akses=${role}`, "DELETE");
 };
