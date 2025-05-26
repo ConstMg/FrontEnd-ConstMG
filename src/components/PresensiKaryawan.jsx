120007d3bfd8e12635fa988354349fe8b269d61f
Moch Rizqi <rizqia417@gmail.com>
2025-05-26 18:19:08 +0700

    fixing table overflow

--- src/components/PresensiKaryawan.jsx
+++ src/components/PresensiKaryawan.jsx
@@ -119,8 +119,8 @@ const PresensiKaryawan = () => {
                     {/* ✅ Tampilkan form atau tampilan sukses */}
                     {showRiwayat && (
                         <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
-                            <div className="w-3/4 h-1/2 bg-white p-6 rounded-2xl shadow-lg z-10">
-                                <div className="flex justify-between items-center mb-4">
+                            <div className="w-3/4 md:h-3/4 bg-white p-6 rounded-2xl shadow-lg z-10 h-dvh overflow-auto relative">
+                                <div className="flex justify-between items-center mb-4 absolute top-4 left-4 right-4">
                                     <h2 className="text-xl font-semibold text-gray-700">
                                         Riwayat Presensi
                                     </h2>
@@ -134,7 +134,7 @@ const PresensiKaryawan = () => {
                                 {isLoading ? (
                                     <p>Loading...</p>
                                 ) : (
-                                    <div className="overflow-auto max-h-[400px]">
+                                    <div className="overflow-auto h-full mt-6">
                                         <table className="min-w-full divide-y divide-gray-200">
                                             <thead className="bg-gray-50">
                                                 <tr>
