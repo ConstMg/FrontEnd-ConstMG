1d8158972057059e312361944a4c94ad33b1fcb3
Kemas Khairunsyah <khairunsyah2714@gmail.com>
2025-07-06 22:44:45 +0700

    update lagi

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -125,7 +125,7 @@ const Home = () => {
                 <div className="h-[48px] inline-block">
                     <Link
                         to="visi-misi"
-                        className="absolute cursor-pointer transition-all bg-amber-800 text-white px-6 py-2 rounded-lg border-amber-500 
+                        className="absolute cursor-pointer transition-all bg-amber-400 text-white px-6 py-2 rounded-lg border-amber-500 
           border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
           active:border-b-[2px] active:brightness-90 active:translate-y-[2px]"
                     >
