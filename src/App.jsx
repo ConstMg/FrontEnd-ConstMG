d2bf963165c8b3a672d90ade28c9e7b9e1d40e02
Kemas Khairunsyah <khairunsyah2714@gmail.com>
2025-07-06 22:38:40 +0700

    update Toast Bottom

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     return (
         <>
             <ToastContainer
-                position="top-right"
+                position="bottom-right"
                 autoClose={1000}
                 theme="colored"
             />
