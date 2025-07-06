2557b760870ff83714b16d9313df355bc40e21c7
Kemas Khairunsyah <khairunsyah2714@gmail.com>
2025-07-06 18:55:56 +0700

    Uji coba deploy via GitHub Actions

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -78,7 +78,6 @@ export function useAuth() {
                 });
             }
             
-
             localStorage.clear();
             setIsLoggedIn(false);
             setTimeout(() => {
